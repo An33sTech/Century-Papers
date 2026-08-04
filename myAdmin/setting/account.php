ab0e7bbda71a04b2b0882c1a1bea07f34be2ca70
Muhammad Anis <anistech.dev@gmail.com>
2026-08-04 17:19:17 +0500

    Enhance account settings form with HTML escaping

--- myAdmin/setting/account.php
+++ myAdmin/setting/account.php
@@ -4,16 +4,11 @@ global $dbF;
 $functions->require_once_custom('setting.class.php');
 $setting    =  new setting();
 
-
-
 $setting->AccountSubmit();
 $accountData = $setting->getAccoutSettingData();
 ?>
 <h4 class="sub_heading borderIfNotabs"><?php echo _uc($_e['Account Setting']); ?></h4>
 
-<?php
-//$id = $_SESSION['_uid'];
-//$WebUser->newAdminUser($id,'account'); ?>
 
 
     <div class="container-fluid">
@@ -24,38 +19,57 @@ $accountData = $setting->getAccoutSettingData();
             <div class="form-group">
                 <label class="col-sm-4 col-md-3 control-label" ><?php echo _uc($_e['Account Name']); ?></label>
                 <div class="col-sm-8 col-md-9">
-                    <?php
-                    $temp = $accountData['acc_name'];
-                    ?>
-                    <input type="text" required="" value="<?php echo $temp; ?>" name="acc_name" id="acc_name" class="form-control">
+                    <input type="text" required value="<?php echo htmlspecialchars($accountData['acc_name'], ENT_QUOTES, 'UTF-8'); ?>" name="acc_name" id="acc_name" class="form-control">
                 </div>
             </div>
 
             <div class="form-group">
                 <label class="col-sm-4 col-md-3 control-label" ><?php echo _uc($_e['Email']); ?></label>
                 <div class="col-sm-8 col-md-9">
-                    <?php
-                    $temp = $accountData['acc_email'];
-                    ?>
-                    <input type="email" required="" value="<?php echo $temp; ?>" name="acc_email" id="acc_email" class="form-control">
+                    <input type="email" required value="<?php echo htmlspecialchars($accountData['acc_email'], ENT_QUOTES, 'UTF-8'); ?>" name="acc_email" id="acc_email" class="form-control">
                 </div>
             </div>
 
             <div class="form-group">
-                <label class="col-sm-4 col-md-3 control-label" ><?php echo _uc($_e['Password']); ?></label>
+                <label class="col-sm-4 col-md-3 control-label"><?php echo _uc($_e['Current Password']); ?></label>
                 <div class="col-sm-8 col-md-9">
-                    <?php
-                    ?>
-                    <input type="text" value="" onChange="passM();" name="password" id="pass" class="form-control" placeholder="<?php echo _uc($_e['Leave Blank If not want to update']); ?>">
+                    <input type="password"
+                           name="current_password"
+                           id="current_password"
+                           class="form-control"
+                           autocomplete="current-password"
+                           placeholder="Required only if changing password">
                 </div>
             </div>
-
+            
             <div class="form-group">
-                <label class="col-sm-4 col-md-3 control-label" ><?php echo _uc($_e['Retype Password']); ?></label>
+                <label class="col-sm-4 col-md-3 control-label"><?php echo _uc($_e['Password']); ?></label>
                 <div class="col-sm-8 col-md-9">
-                    <?php
-                    ?>
-                    <input type="text" value="" onChange="passM();" onkeyup="passM();" name="retype_password" id="rpass" class="form-control">
+                    <input type="password"
+                           value=""
+                           onChange="passM();"
+                           onkeyup="passM();"
+                           name="password"
+                           id="pass"
+                           class="form-control"
+                           minlength="8"
+                           autocomplete="new-password"
+                           placeholder="<?php echo _uc($_e['Leave Blank If not want to update']); ?>">
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <label class="col-sm-4 col-md-3 control-label"><?php echo _uc($_e['Retype Password']); ?></label>
+                <div class="col-sm-8 col-md-9">
+                    <input type="password"
+                           value=""
+                           onChange="passM();"
+                           onkeyup="passM();"
+                           name="retype_password"
+                           id="rpass"
+                           class="form-control"
+                           minlength="8"
+                           autocomplete="new-password">
                     <div id="pm"></div>
                 </div>
             </div>
