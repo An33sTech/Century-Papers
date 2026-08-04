6044fda88e6692fb52381ac9555d619fe335783f
Muhammad Anis <anistech.dev@gmail.com>
2026-08-04 17:16:44 +0500

    Enhance logout functionality with secure cookie handling
    Updated logout process to clear session and cookies securely.

--- myAdmin/logout.php
+++ myAdmin/logout.php
@@ -1,23 +1,39 @@
-<?php require("global.php");
-unset($_SESSION);
+<?php
+require("global.php");
+
+if (!empty($_SESSION['_uid'])) {
+    $sql = "UPDATE `accounts`
+            SET `acc_session` = ''
+            WHERE `acc_id` = ?";
+
+    $dbF->setRow($sql, [(int)$_SESSION['_uid']]);
+}
+
+$_SESSION = [];
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+
+    setcookie(session_name(), '', [
+        'expires'  => time() - 3600,
+        'path'     => $params['path'] ?? '/',
+        'domain'   => $params['domain'] ?? '',
+        'secure'   => true,
+        'httponly' => true,
+        'samesite' => 'Lax'
+    ]);
+}
+
+setcookie('_uid', '', [
+    'expires'  => time() - 3600,
+    'path'     => '/',
+    'secure'   => true,
+    'httponly' => true,
+    'samesite' => 'Lax'
+]);
+
 session_unset();
 session_destroy();
-@setcookie(session_name(), '', time()-55000, '/');
-if(isset($_COOKIE[session_name()])) {
-    setcookie(session_name(), '', time()-55000, '/');
-}
 
-// Destroy all cookies.
-if (isset($_SERVER['HTTP_COOKIE'])) {
-    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
-    foreach($cookies as $cookie) {
-        $parts = explode('=', $cookie);
-        $name = trim($parts[0]);
-        setcookie($name, '', time()-1000);
-        setcookie($name, '', time()-1000, '/');
-    }
-    $_SERVER['HTTP_COOKIE'] = '';
-}
-
-header("location:index.php");
-?>
+header("Location: " . WEB_ADMIN_URL, true, 302);
+exit;
